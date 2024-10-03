c52b4d422ad10862e553b96dea80f5163d1c2d74
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2024-10-03 22:32:04 +0000

    Fix _version.py

---
+++ geoips_tropics/_version.py
@@ -0,0 +1,8 @@
+# # # This source code is protected under the license referenced at
+# # # https://github.com/NRLMMD-GEOIPS.
+
+# DO NOT EDIT
+# managed by poetry-dynamic-versioning
+__version__ = "0.0.0"
+__version_tuple__ = (0, 0, 0)
+(geoips-github) (base) 
